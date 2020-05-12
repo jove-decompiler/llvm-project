jove-decompiler/llvm-project

commit 835e01937e7b35c4bce3cf37b4625e1e6195a306

[analyzer] SATestBuild.py: Allow comments in run_static_analyzer.cmd.
Because those can get really weird sometimes.

clang/utils/analyzer/SATestBuild.py
@@ -298,7 +298,7 @@ def runScanBuild(Args, Dir, SBOutputDir, PBuildLogFile):
         SBPrefix = "scan-build " + SBOptions + " "
         for Command in SBCommandFile:
             Command = Command.strip()
-            if len(Command) == 0:
+            if len(Command) == 0 or Command.startswith("#"):
                 continue
 
             # Custom analyzer invocation specified by project.

commit d8c632dd27afe636cb4052cfbd8dca3deff49395

[analyzer] SATestBuild.py: Fix support for #NOPREFIX.
Regressed in 835e019.

clang/utils/analyzer/SATestBuild.py
@@ -298,7 +298,7 @@ def runScanBuild(Args, Dir, SBOutputDir, PBuildLogFile):
         SBPrefix = "scan-build " + SBOptions + " "
         for Command in SBCommandFile:
             Command = Command.strip()
-            if len(Command) == 0 or Command.startswith("#"):
+            if len(Command) == 0:
                 continue
 
             # Custom analyzer invocation specified by project.
@@ -309,6 +309,9 @@ def runScanBuild(Args, Dir, SBOutputDir, PBuildLogFile):
                 ExtraEnv['OUTPUT'] = SBOutputDir
                 ExtraEnv['CC'] = Clang
                 ExtraEnv['ANALYZER_CONFIG'] = generateAnalyzerConfig(Args)
+                continue
+
+            if Command.startswith("#"):
                 continue
 
             # If using 'make', auto imply a -jX argument