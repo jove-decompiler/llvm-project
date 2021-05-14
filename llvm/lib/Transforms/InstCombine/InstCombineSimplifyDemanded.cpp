jove-decompiler/llvm-project

commit 665fca90e8ef45997a73a7b9d18075cca7d7e280

[InstCombine] improve demanded bits analysis of left-shifted operand
If we don't demand high bits, then we also don't care about those
high bits of a left-shift operand regardless of shift amount.
I noticed the sext/trunc pattern in a motivating example.
It seems like there should be a low-bits with right-shift sibling,
but I haven't looked at that yet.

https://alive2.llvm.org/ce/z/JuS6jc
https://rise4fun.com/Alive/Trm (not sure how to use 'width' with Alive1)
https://alive2.llvm.org/ce/z/gRadbF

Differential Revision: https://reviews.llvm.org/D101489

llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
@@ -559,6 +559,14 @@ Value *InstCombinerImpl::SimplifyDemandedUseBits(Value *V, APInt DemandedMask,
           return UndefValue::get(I->getType());
       }
     } else {
+      // This is a variable shift, so we can't shift the demand mask by a known
+      // amount. But if we are not demanding high bits, then we are not
+      // demanding those bits from the pre-shifted operand either.
+      if (unsigned CTLZ = DemandedMask.countLeadingZeros()) {
+        APInt DemandedFromOp(APInt::getLowBitsSet(BitWidth, BitWidth - CTLZ));
+        if (SimplifyDemandedBits(I, 0, DemandedFromOp, Known, Depth + 1))
+          return I;
+      }
       computeKnownBits(I, Known, Depth, CxtI);
     }
     break;

llvm/test/Transforms/InstCombine/shl-demand.ll
@@ -3,8 +3,8 @@
 
 define i16 @sext_shl_trunc_same_size(i16 %x, i32 %y) {
 ; CHECK-LABEL: @sext_shl_trunc_same_size(
-; CHECK-NEXT:    [[CONV:%.*]] = sext i16 [[X:%.*]] to i32
-; CHECK-NEXT:    [[SHL:%.*]] = shl i32 [[CONV]], [[Y:%.*]]
+; CHECK-NEXT:    [[CONV1:%.*]] = zext i16 [[X:%.*]] to i32
+; CHECK-NEXT:    [[SHL:%.*]] = shl i32 [[CONV1]], [[Y:%.*]]
 ; CHECK-NEXT:    [[T:%.*]] = trunc i32 [[SHL]] to i16
 ; CHECK-NEXT:    ret i16 [[T]]
 ;
@@ -16,8 +16,8 @@ define i16 @sext_shl_trunc_same_size(i16 %x, i32 %y) {
 
 define i5 @sext_shl_trunc_smaller(i16 %x, i32 %y) {
 ; CHECK-LABEL: @sext_shl_trunc_smaller(
-; CHECK-NEXT:    [[CONV:%.*]] = sext i16 [[X:%.*]] to i32
-; CHECK-NEXT:    [[SHL:%.*]] = shl i32 [[CONV]], [[Y:%.*]]
+; CHECK-NEXT:    [[CONV1:%.*]] = zext i16 [[X:%.*]] to i32
+; CHECK-NEXT:    [[SHL:%.*]] = shl i32 [[CONV1]], [[Y:%.*]]
 ; CHECK-NEXT:    [[T:%.*]] = trunc i32 [[SHL]] to i5
 ; CHECK-NEXT:    ret i5 [[T]]
 ;
@@ -26,6 +26,8 @@ define i5 @sext_shl_trunc_smaller(i16 %x, i32 %y) {
   %t = trunc i32 %shl to i5
   ret i5 %t
 }
+
+; negative test - demanding 1 high-bit too many to change the extend
 
 define i17 @sext_shl_trunc_larger(i16 %x, i32 %y) {
 ; CHECK-LABEL: @sext_shl_trunc_larger(
@@ -42,8 +44,8 @@ define i17 @sext_shl_trunc_larger(i16 %x, i32 %y) {
 
 define i32 @sext_shl_mask(i16 %x, i32 %y) {
 ; CHECK-LABEL: @sext_shl_mask(
-; CHECK-NEXT:    [[CONV:%.*]] = sext i16 [[X:%.*]] to i32
-; CHECK-NEXT:    [[SHL:%.*]] = shl i32 [[CONV]], [[Y:%.*]]
+; CHECK-NEXT:    [[CONV1:%.*]] = zext i16 [[X:%.*]] to i32
+; CHECK-NEXT:    [[SHL:%.*]] = shl i32 [[CONV1]], [[Y:%.*]]
 ; CHECK-NEXT:    [[T:%.*]] = and i32 [[SHL]], 65535
 ; CHECK-NEXT:    ret i32 [[T]]
 ;
@@ -52,6 +54,8 @@ define i32 @sext_shl_mask(i16 %x, i32 %y) {
   %t = and i32 %shl, 65535
   ret i32 %t
 }
+
+; negative test - demanding a bit that could change with sext
 
 define i32 @sext_shl_mask_higher(i16 %x, i32 %y) {
 ; CHECK-LABEL: @sext_shl_mask_higher(
@@ -66,9 +70,11 @@ define i32 @sext_shl_mask_higher(i16 %x, i32 %y) {
   ret i32 %t
 }
 
+; May need some, but not all of the bits set by the 'or'.
+
 define i32 @set_shl_mask(i32 %x, i32 %y) {
 ; CHECK-LABEL: @set_shl_mask(
-; CHECK-NEXT:    [[Z:%.*]] = or i32 [[X:%.*]], 196609
+; CHECK-NEXT:    [[Z:%.*]] = or i32 [[X:%.*]], 65537
 ; CHECK-NEXT:    [[S:%.*]] = shl i32 [[Z]], [[Y:%.*]]
 ; CHECK-NEXT:    [[R:%.*]] = and i32 [[S]], 65536
 ; CHECK-NEXT:    ret i32 [[R]]

llvm/test/Transforms/InstCombine/rotate.ll
@@ -443,7 +443,8 @@ define i8 @rotate_right_commute_8bit_unmasked_shl(i32 %v, i32 %shift) {
 ; CHECK-NEXT:    [[TMP1:%.*]] = trunc i32 [[SHIFT:%.*]] to i8
 ; CHECK-NEXT:    [[TMP2:%.*]] = and i8 [[TMP1]], 3
 ; CHECK-NEXT:    [[TMP3:%.*]] = trunc i32 [[V:%.*]] to i8
-; CHECK-NEXT:    [[CONV2:%.*]] = call i8 @llvm.fshr.i8(i8 [[TMP3]], i8 [[TMP3]], i8 [[TMP2]])
+; CHECK-NEXT:    [[TMP4:%.*]] = trunc i32 [[V]] to i8
+; CHECK-NEXT:    [[CONV2:%.*]] = call i8 @llvm.fshr.i8(i8 [[TMP3]], i8 [[TMP4]], i8 [[TMP2]])
 ; CHECK-NEXT:    ret i8 [[CONV2]]
 ;
   %and = and i32 %shift, 3

commit 86236a4c6ef7e6ea91ac342fd36fce23cb2b911a

[InstCombine] drop poison flags when simplifying 'shl' based on demanded bits
As with other transforms in demanded bits, we must be careful not to
wrongly propagate nsw/nuw if we are reducing values leading up to the shift.

This bug was introduced with 665fca90e8ef and leads to the miscompile
shown in:
https://llvm.org/PR50341

llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
@@ -575,8 +575,11 @@ Value *InstCombinerImpl::SimplifyDemandedUseBits(Value *V, APInt DemandedMask,
       // demanding those bits from the pre-shifted operand either.
       if (unsigned CTLZ = DemandedMask.countLeadingZeros()) {
         APInt DemandedFromOp(APInt::getLowBitsSet(BitWidth, BitWidth - CTLZ));
-        if (SimplifyDemandedBits(I, 0, DemandedFromOp, Known, Depth + 1))
+        if (SimplifyDemandedBits(I, 0, DemandedFromOp, Known, Depth + 1)) {
+          // We can't guarantee that nsw/nuw hold after simplifying the operand.
+          I->dropPoisonGeneratingFlags();
           return I;
+        }
       }
       computeKnownBits(I, Known, Depth, CxtI);
     }

llvm/test/Transforms/InstCombine/shl-demand.ll
@@ -89,7 +89,7 @@ define i32 @set_shl_mask(i32 %x, i32 %y) {
 
 define i8 @must_drop_poison(i32 %x, i32 %y)  {
 ; CHECK-LABEL: @must_drop_poison(
-; CHECK-NEXT:    [[S:%.*]] = shl nuw nsw i32 [[X:%.*]], [[Y:%.*]]
+; CHECK-NEXT:    [[S:%.*]] = shl i32 [[X:%.*]], [[Y:%.*]]
 ; CHECK-NEXT:    [[T:%.*]] = trunc i32 [[S]] to i8
 ; CHECK-NEXT:    ret i8 [[T]]
 ;