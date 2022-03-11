jove-decompiler/llvm-project

commit a4a853a4569c6115505107e42f1d5a8faf073a8c

Conditional include of missing headers under Z3

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,10 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
+#include "llvm/ADT/SmalLString.h"
 #include "llvm/ADT/Twine.h"
+
+#include <set>
 
 #include <z3.h>
 

commit 90828e2ae6e3d2c4f3e576ab951d827c12a5e158

[analyzer] Fix include typo introduced by a4a853a

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,7 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
-#include "llvm/ADT/SmalLString.h"
+#include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/Twine.h"
 
 #include <set>